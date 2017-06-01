6d4fb43d468d3b0dcf19047cdc03617b53df9911
Kanyango <kariukikanyango@gmail.com>
2017-06-01 08:47:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 exports.hostname = process.env.hostname || '127.0.0.1';
 exports.port = process.env.PORT || 8300;
 exports.mongodb = {
-	uri: 'mongodb://127.0.0.1:27017/llabsdb'
-	//uri: ' mongodb://DaveBuddy:bmw760li@ds015915.mlab.com:15915/buddyappdb'
+	//uri: 'mongodb://127.0.0.1:27017/llabsdb'
+	uri: 'mongodb://kanyango:kanyango@ds161001.mlab.com:61001/llabsdb'
 };
 exports.secret = 'b7TY?>m6wl_i/<';
 
